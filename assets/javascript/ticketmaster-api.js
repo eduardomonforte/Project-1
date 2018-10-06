cb93a7018102d882832d44d8a995d17620849eae
Eduardo Monforte <eduardomonforte@gmail.com>
2018-10-06 13:36:18 -0500

    newest changes

--- assets/javascript/ticketmaster-api.js
+++ assets/javascript/ticketmaster-api.js
@@ -1,1 +1,21 @@
 console.log("Ready.");
+
+$(document).ready(function() {
+
+    console.log("Even more ready.")
+
+    var responseSize = (3).toString()
+    var apiKey = "8qqzR9xAATp2Wyh7mCELVegociPYsEVT"
+
+    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + apiKey + "&countryCode=MX&size=" + responseSize
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+
+        console.log(response);
+
+    });
+
+});
