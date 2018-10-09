85d16ed221fe8a339b505fb562af24bc9b7cd293
adriansg85 <adriansalinasg@gmail.com>
2018-10-08 22:53:10 -0500

    monday work advance

--- assets/javascript/googleMaps.js
+++ assets/javascript/googleMaps.js
@@ -2,8 +2,11 @@
 function initMap() {
     // The location of Mexico
     var mexico = {
-        lat: 23.368116,
-        lng: -102.268791
+        lat: 19.3390515900001,
+        lng: -99.06427109
+
+        //23.368116,
+        //-102.268791
     };
     // The map, centered at Mexico
     var map = new google.maps.Map(
@@ -29,3 +32,28 @@ var config = {
     messagingSenderId: "9954661068"
 };
 firebase.initializeApp(config);
+
+
+function prueba() {
+    $.ajax({
+        url: "http://gaia.inegi.org.mx/sakbe_v3.1/libre",
+        method: "POST",
+        data: {
+            key: "rZMkTZuI-ugbU-t1Y5-zCHg-0ZOETYT3r0HG",
+            id_i: 1,
+            source_i: 254,
+            target_i: 255,
+            id_f: 775167,
+            source_f: 250771,
+            target_f: 687808,
+            v: 1,
+            type: "json",
+            proj: "GRS80",
+
+        }
+
+    }).done(function (response) {
+        console.log(response.data.geojson);
+
+    });
+}
